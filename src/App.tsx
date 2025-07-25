1055e285d5fce56fbf1d04911b82cbd3b3b76f59
manduakash <manduakash@gmail.com>
2025-07-25 14:22:58 +0530

    akash

--- src/App.tsx
+++ src/App.tsx
@@ -154,10 +154,10 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <ToastProvider>
-        <Router>
+        {/* <Router> */}
         <BrowserRouter>
           <AppContent />
-        </Router>
+        {/* </Router> */}
         </BrowserRouter>
       </ToastProvider>
     </div>
